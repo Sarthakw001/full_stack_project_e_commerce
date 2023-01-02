7847f0703327ccdf93bcf8818fbe3c93b9bf7baf
Sarthakw001 <sarthakw01@gmail.com>
2023-01-02 18:20:39 +0530

    added morgan,cors,cookie-parser

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,17 @@
-import express from 'express';
+import express from "express";
+import cookieparser from "cookie-parser";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieparser());
+
+app.use(morgan("tiny"));
+
+
 
 export default app;
