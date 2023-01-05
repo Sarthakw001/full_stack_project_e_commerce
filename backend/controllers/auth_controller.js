560793a33bfebf4d5455cad93c05b253fc2d3bdf
Sarthakw001 <sarthakw01@gmail.com>
2023-01-05 11:28:22 +0530

    Auth - Login , Logout

--- backend/controllers/auth_controller.js
+++ backend/controllers/auth_controller.js
@@ -38,10 +38,65 @@ export const signUp = asyncHandler(async (req, res) => {
   // console.log(user);
   user.password = undefined;
 
-  res.cookie("token",token,cookieOptions);
+  res.cookie("token", token, cookieOptions);
   res.status(200).json({
-    success:true,
+    success: true,
     token,
-    user
+    user,
+  });
+});
+
+/***********************************************************
+ * @SIGNIN
+ * @route http://localhost:/api/auth/login
+ * @description User Login controller for creating Logging in
+ * @parameters email,password
+ * @return User Object
+ ***********************************************************/
+
+export const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomError("Please Fill all details", 400);
+  }
+
+  const existingUser = await User.findOne({ email }).select("+password");
+  if (existingUser) {
+    const isPassword = await existingUser.comparePassword(password);
+    if (isPassword) {
+      const token = existingUser.getJwtToken();
+      existingUser.password = undefined;
+      res.cookie("token", token, cookieOptions);
+      return res.status(200).json({
+        success: true,
+        token,
+        existingUser,
+      });
+    } else {
+      throw new CustomError("Invalid Credentials", 400);
+    }
+  } else {
+    throw new CustomError("Invalid Credentials", 400);
+  }
+});
+
+/***********************************************************
+ * @LOGOUT
+ * @route http://localhost:/api/auth/loGOUT
+ * @description User LogOut by clearing user cookies
+ * @parameters
+ * @return success message
+ ***********************************************************/
+
+export const logout = asyncHandler(async (_req, res) => {
+  // res.clearCookie();
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    message: "Logged Out",
   });
 });
